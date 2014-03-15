50d73e8cf4f9eeb3923f50e2043bf8909bd96840
Ben Meier <benmeier42@gmail.com>
2014-03-15 03:01:13 +0200

    initial commit

---
+++ rebuild_img_listing.rb
@@ -0,0 +1,48 @@
+require 'yaml'
+require 'json'
+
+# path stuff
+current_dir = File.dirname(__FILE__)
+
+# load config
+cnf = YAML::load_file(File.join(current_dir, 'config.yml'))
+puts cnf
+
+# time stamp array
+tsa = []
+
+# create img dir
+Dir.mkdir(File.join(cnf['img_dir'])) if not Dir.exists?(cnf['img_dir'])
+
+Dir.glob(File.join(cnf['img_dir'], '*.jpg')) do |f|
+    m = File.basename(f).match(/img(\d*)\.jpg/)
+    if m
+        tsa << m[1].to_i
+    end
+end
+
+# sort timestamps in reverse
+tsa = tsa.uniq.sort.reverse
+
+# select top n
+tsa = tsa[0, cnf['num_img']]
+
+# delete those that aren't needed
+Dir.glob(File.join(cnf['img_dir'], '*.jpg')) do |f|
+    m = File.basename(f).match(/((img)|(thumb))(\d*)\.jpg/)
+    if not m or not tsa.include? m[4].to_i
+        File.delete(f)
+        puts "deleting #{f}"
+    end
+end
+
+# build images javascript
+imgs = tsa.map {|x| ["img#{x}.jpg","thumb#{x}.jpg"]}
+js = "var imagearray = #{JSON.dump(imgs)};"
+
+# create js dir
+Dir.mkdir(File.join(cnf['js_dir'])) if not Dir.exists?(cnf['js_dir'])
+
+# write out
+outf = File.join(cnf['js_dir'], 'dataset.js')
+File.open(outf, 'w') {|f| f.write(js) }
